90c351ffd0debd17a27919fb1fdaaafb48d926f9
Emrah <seren.emrah@gmail.com>
2018-06-30 11:54:03 +0300

    update1

--- server.js
+++ server.js
@@ -1,44 +1,83 @@
+// Nuh Emrah Seren
+// Noah^s Ark - seren@gmail.com
+// June 2018 - Istanbul 
+
+// Express lib.
 var express = require('express');
+// File path etc. __dirname  lib.
 var path = require('path');
+// BodyParser lib.
 var bodyParser = require('body-parser');
+// FileRead lib.
+var fs = require('fs');
+
+
+
+// CREATE app by Express
 var app = express();
 
 
+// SET app view pages file type by Express
 app.set('view engine', 'ejs');
+// SET view pages folder 
 app.set('views', path.join(__dirname, 'views'));
 
+// Set middleware param to parse from form input parameter
 app.use(bodyParser());
 
+// Global list variable
 var todoItems = [
             {id : 1, desc : 'item 11'},
             {id : 2, desc : 'item 22'},
             {id : 3, desc : 'item 33'}
             ];
 
+
+// GET - Rote base folder and send Item list to the FrondEnd Side
 app.get('/', function(req, res){
     res.render('home', {
         title : "TITLE",
         items : todoItems
     });
-    
     //res.send("<h2>Hello, Welcome Node Express</h2>")
-    
 });
 
+
+// POST - read input from post 
 app.post('/add', function(req, res){ 
     var newItem = req.body.newItem;
-    todoItems.push({id:9, desc:newItem});
+    todoItems.push({id:0, desc:newItem});
     
     res.redirect('/');
 });
 
+
+// GET - Read JSON file from local post RESTFull Service
+app.get('/read', function(req, res){
+    fs.readFile('./files/data.json', function(err, data) {
+        if(err) throw err;
+        var array = data.toString().split("\n");
+        res.send(array);
+    });
+});
+
+// GET Render view page example   ----------------------
+app.get('/login', function(req, res){
+    res.render('login');
+})
+
+
+
+// GET - GET URL Parameter ------------------------------
 app.get('api/user/:id', function(req, res){
     var uid = req.param.id;
     console.log("UID : "+uid);
 })
-app.get('/login', function(req, res){
-    res.render('login');
-})
+
+
+// SETUP server listenner and add port. ----------------
 app.listen(1111, function(req, res){
    console.log("1111 port running"); 
 });
+
+
